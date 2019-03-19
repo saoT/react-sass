53a95cc794ce1cc1e15a8115bda8e37532d0053e
saraguerric <sara.guerric@evergig.com>
2019-03-19 12:40:41 +0100

    Add : sass

--- src/App.js
+++ src/App.js
@@ -1,28 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+import './style.scss';
+
+const jsonArray = [
+  {
+    name : 'Pedro'
+  },
+  {
+    name : 'Marie'
+  }
+]
+
+// Les components ce sont des prototypes defini par une class en ES6
+// Ils ont donc tous un contructor
+
+// LE LIFECYCLE
+// Etapes de base pour chaque component APPELE UNE SEUL ET UNIQUE FOIS
+// componentWillMount
+// componentDidMount 
+// render -> la methode qui permet d'afficher le contenu HTML du compoennt
+
+// Etapes declenchées par le setState A CHAQUE FOIS
+// componentWillUpdate
+// componentDidUpdate
+// render -> la methode qui permet d'afficher le contenu HTML du compoennt
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  constructor () {
+    super ();
+
+    this.state = {
+      salutations : 'Bonjour',
+      peoples : jsonArray
+    }
   }
+
+  componentDidMount() {
+    this.setState({salutations : 'Hello'})
+  }
+
+  render () {
+    const that = this;
+    return (
+      <div id="app">
+
+        {
+          this.state.peoples.map( function (person, i) {
+            return (
+              <div className="saluations" key={i}>
+                {that.state.salutations} {person.name}
+
+                 <div className="alert">alert</div>
+                 <div className="important">important</div>
+                 <div className="vip-alert">vip-alert</div>
+                 <div className="small-margin">Je suis une mixin</div>
+                 <div className="medium-margin">Je suis une mixin</div>
+                 <div className="large-margin">Je suis une mixin</div>
+
+              </div>
+            )
+          })
+        }
+
+      </div>
+    )
+  }
+ 
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
